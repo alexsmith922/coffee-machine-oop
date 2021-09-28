f7b047684242fc3fa609339b605ded094857ef4c
alexsmith922 <10asmith@gmail.com>
2021-09-27 21:15:09 -0500

    added while loop for recursion

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+drink_title = MenuItem()
+drinks = Menu()
+is_on = True
+report = CoffeeMaker()
+money_machine = MoneyMachine()
+
+while is_on:
+    drink_choice = input("What would you like to drink?" + " " + drinks.get_items() )
+    if drink_choice == "report":
+        report.report()
+        money_machine.report()
+    elif drink_choice == "off":
+        print("Shutting down...")
+        is_on = False
+    else:
+        report.is_resource_sufficient(drink_title.name)
